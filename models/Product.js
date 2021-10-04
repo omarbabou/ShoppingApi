518710f0673681e45ed323987f93e194ef5dc89d
= <obabou50@gmail.com>
2021-10-04 09:02:14 -0700

    5th commit

--- models/Product.js
+++ models/Product.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
-    desc: { type: String, required: true, unique: true },
+    desc: { type: String, required: true },
     img: { type: String, required: true },
-    categories: { type: String, required: true },
-    size: { type: String, required: true },
-    color: { type: String, required: true },
-    price: { type: String, required: true },
+    categories: { type: Array },
+    size: { type: String },
+    color: { type: String },
+    price: { type: Number, required: true },
   },
   { timestamps: true }
 );
