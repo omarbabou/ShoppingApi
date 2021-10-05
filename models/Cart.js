800eb91ec0c808d2a48643ef352c2a32869ece63
= <obabou50@gmail.com>
2021-10-04 22:37:30 -0700

    morning commit

--- models/Cart.js
+++ models/Cart.js
@@ -2,13 +2,18 @@ const mongoose = require("mongoose");
 
 const CartSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true, unique: true },
-    desc: { type: String, required: true },
-    img: { type: String, required: true },
-    categories: { type: Array },
-    size: { type: String },
-    color: { type: String },
-    price: { type: Number, required: true },
+    userId: { type: String, required: true },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
   },
   { timestamps: true }
 );
