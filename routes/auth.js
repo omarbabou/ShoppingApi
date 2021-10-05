473feb26441a378889f25e6688d32ad0421d3937
= <obabou50@gmail.com>
2021-10-05 00:48:05 -0700

    This is a new commit for what I originally planned to be an amendmend

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+<<<<<<< HEAD
+const jwt = require("jsonwebtoken");
+=======
+>>>>>>> 800eb91ec0c808d2a48643ef352c2a32869ece63
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -23,6 +27,39 @@ router.post("/register", async (req, res) => {
 
 // LOGIN
 
+<<<<<<< HEAD
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong credentials!");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong credentials!");
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user._isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({ ...others, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+=======
 router.post("/login", async (req, res) => {});
+>>>>>>> 800eb91ec0c808d2a48643ef352c2a32869ece63
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
 
+<<<<<<< HEAD
+// router.put("/:id")
+
+=======
+>>>>>>> 800eb91ec0c808d2a48643ef352c2a32869ece63
 // router.get("/usertest", (req, res) => {
 //   res.send("user test is successfull");
 // });
