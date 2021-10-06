161e73f7629bf5a2e44ab3566206d6ac664e8be4
= <obabou50@gmail.com>
2021-10-06 00:21:46 -0700

    morning commit

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,29 @@
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+
 const router = require("express").Router();
 
-// router.put("/:id")
+// UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+
+  try {
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // router.get("/usertest", (req, res) => {
 //   res.send("user test is successfull");
